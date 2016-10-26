c98ecc8cf11198aec95374ff72b9355781d96730
Ataul Munim <hello@ataulm.com>
2016-10-26 15:19:29 +0100

    Add beat to Note

--- core/src/main/java/com/ataulm/notes/Note.java
+++ core/src/main/java/com/ataulm/notes/Note.java
@@ -3,9 +3,12 @@ package com.ataulm.notes;
 public class Note {
 
     private final Pitch pitch;
+    private final Beat beat;
 
-    public Note(Pitch pitch) {
+    public Note(Pitch pitch, Beat beat) {
         this.pitch = pitch;
+        this.beat = beat;
     }
+
 }
 
