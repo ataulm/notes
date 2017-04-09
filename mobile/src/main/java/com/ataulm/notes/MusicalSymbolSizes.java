5ce09be894c5ddb0dfc2ec752f5282f268d6a382
Ataul Munim <hello@ataulm.com>
2017-04-09 13:14:06 +0100

    Add symbol size container class

---
+++ mobile/src/main/java/com/ataulm/notes/MusicalSymbolSizes.java
@@ -0,0 +1,17 @@
+package com.ataulm.notes;
+
+class MusicalSymbolSizes {
+
+    final Size note;
+    final Size sharp;
+    final Size flat;
+    final Size natural;
+
+    MusicalSymbolSizes(Size note, Size sharp, Size flat, Size natural) {
+        this.note = note;
+        this.sharp = sharp;
+        this.flat = flat;
+        this.natural = natural;
+    }
+
+}
